2e7812717041f5e4908b5a9b17a4bfa92bf10dc5
Ravie <ravijkee@yandex.ru>
2019-04-04 10:59:39 +0300

    Reformat code (part 2)

--- Swing/src/com/netcracker/educentr/nn/group2/boris/Swing.java
+++ Swing/src/com/netcracker/educentr/nn/group2/boris/Swing.java
@@ -12,29 +12,29 @@ public class Swing extends JFrame {
         super("Library");
         setSize(1280, 720);
         setLocation(150, 100);
-        setDefaultCloseOperation( EXIT_ON_CLOSE );
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
 
-        BookModel m=new BookModel();
-        table=new JTable(m);
-        JScrollPane jScrollPane=new JScrollPane(table);
+        BookModel m = new BookModel();
+        table = new JTable(m);
+        JScrollPane jScrollPane = new JScrollPane(table);
         add(jScrollPane);
-        JPanel grid = new JPanel(new GridLayout(1, 2, 5, 0) );
+        JPanel grid = new JPanel(new GridLayout(1, 2, 5, 0));
 
         JButton btnAdd = new JButton("Add");
-        btnAdd.addActionListener( new BtnAddClick());
+        btnAdd.addActionListener(new BtnAddClick());
         grid.add(btnAdd);
 
         JButton btnEdit = new JButton("Edit");
-        btnEdit.addActionListener( new BtnEditClick());
+        btnEdit.addActionListener(new BtnEditClick());
         grid.add(btnEdit);
 
         JButton btnRemove = new JButton("Remove");
-        btnRemove.addActionListener( new BtnRemoveClick());
+        btnRemove.addActionListener(new BtnRemoveClick());
         grid.add(btnRemove);
 
-        JPanel flow = new JPanel(new FlowLayout(FlowLayout.RIGHT ));
+        JPanel flow = new JPanel(new FlowLayout(FlowLayout.RIGHT));
         flow.add(grid);
-        add(flow, BorderLayout.SOUTH );
+        add(flow, BorderLayout.SOUTH);
         setVisible(true);
     }
 
@@ -42,14 +42,14 @@ public class Swing extends JFrame {
         @Override
         public void actionPerformed(ActionEvent e) {
             int result = JOptionPane.showConfirmDialog(Swing.this,
-                            "Подтвердите удаление "+table.getSelectedRows().length+" строк(и)",
-                            TITLE_confirm,
-                            JOptionPane.YES_NO_OPTION,
-                            JOptionPane.WARNING_MESSAGE);
+                    "Подтвердите удаление " + table.getSelectedRows().length + " строк(и)",
+                    TITLE_confirm,
+                    JOptionPane.YES_NO_OPTION,
+                    JOptionPane.WARNING_MESSAGE);
             if (result == JOptionPane.YES_OPTION) {
                 int[] selection = table.getSelectedRows();
                 for (int i = selection.length - 1; i >= 0; i--) {
-                    ((BookModel)table.getModel()).removeBook(selection[i]);
+                    ((BookModel) table.getModel()).removeBook(selection[i]);
                 }
             }
         }
@@ -65,7 +65,7 @@ public class Swing extends JFrame {
     private class BtnEditClick implements ActionListener {
         @Override
         public void actionPerformed(ActionEvent e) {
-            if(table.getSelectedRow() > -1 && table.getSelectedRowCount() == 1) {
+            if (table.getSelectedRow() > -1 && table.getSelectedRowCount() == 1) {
                 SwingUtilities.invokeLater(() -> new BookAdd(table, false));
             }
         }
